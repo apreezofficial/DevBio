10650c5fc15a60f45537873d873d560ee9dc19e3
apreezofficial <apreezofficial@gmail.com>
2025-06-16 06:17:00 +0100

    update

---
+++ docs/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+      <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="/tailwind.js"></script>
+      <link rel="stylesheet" href="./includes/font-awesome/css/all.css">
+            <link rel="stylesheet" href="./includes/css/animation.css">
+                <script src="/includes/js/theme.js"></script>
+  <script>
+  tailwind.config = { darkMode: 'class' }
+</script>
+</head>
+<body class="bg-white text-gray-800 dark:bg-[#0D0D0D] dark:text-gray-100 transition-colors duration-300">
+  <?php include '../includes/nav.php';?>
+  </body>
+  </html>
